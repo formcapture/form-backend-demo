834e7c4cb02e6b638fd58e7ae5980dd53b33ecc4
Andre Henn <henn@terrestris.de>
2025-08-05 16:53:31 +0200

    fix: cleanup initial SQL for role / user creation

--- postgres/init_data/99_init_postgrest.sql
+++ postgres/init_data/99_init_postgrest.sql
@@ -1,24 +1,20 @@
--- anonymous user for postgrest. This user will not have
--- any permission whatsoever, but will be able to connect.
-create role web_anon noinherit nologin;
+-- -- anonymous user for postgrest. This user will not have
+-- -- any permission whatsoever, but will be able to connect.
+CREATE ROLE web_anon NOINHERIT NOLOGIN;
+--
+-- -- user for the formbackend. This user must have full access
+-- -- to the tables and schemas the formbackend needs to interact with.
+CREATE ROLE formbackend NOINHERIT NOLOGIN;
 
--- user for the formbackend. This user must have full access
--- to the tables and schemas the formbackend needs to interact with.
-create role formbackend noinherit nologin;
+CREATE ROLE authenticator LOGIN NOINHERIT NOCREATEDB NOCREATEROLE NOSUPERUSER;
 
-grant USAGE on schema sampledata to formbackend;
-grant SELECT, INSERT, UPDATE, DELETE on all tables in schema sampledata to formbackend;
-grant all on all sequences in schema sampledata to formbackend;
+GRANT USAGE ON SCHEMA sampledata TO formbackend;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA sampledata TO formbackend;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA sampledata TO formbackend;
 
-grant SELECT on all tables in schema sampledata to web_anon;
-grant SELECT on all sequences in schema sampledata to web_anon;
-grant USAGE on schema sampledata to web_anon;
+GRANT USAGE ON SCHEMA sampledata TO web_anon;
+GRANT SELECT ON ALL TABLES IN SCHEMA sampledata TO web_anon;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA sampledata TO web_anon;
 
--- Apply session timeouts for roles
-ALTER ROLE formbackend SET statement_timeout TO '10s';
-ALTER ROLE web_anon SET statement_timeout TO '1s';
-
--- password will be replaced by init script
-create role authenticator inherit login password 'letmein';
-grant web_anon to authenticator;
-grant formbackend to authenticator;
+GRANT web_anon TO authenticator;
+GRANT formbackend TO authenticator;
